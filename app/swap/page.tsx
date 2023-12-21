8164d6b2c2fdcb391e06ad02e642d360efb73903
aditya-kreatorverse <aditya@kreatorverse.com>
2023-12-21 22:53:17 +0530

    swap page modification

--- app/swap/page.tsx
+++ app/swap/page.tsx
@@ -3,12 +3,32 @@
 import Paper from '@mui/material/Paper';
 import PageTitle from '../_components/PageTitle/PageTitle';
 import Box from '@mui/material/Box';
-import { Card, CardContent, MenuItem, Select, SelectChangeEvent, Stack, TextField } from '@mui/material';
+import { Card, CardContent, Divider, MenuItem, Select, SelectChangeEvent, Stack, TextField } from '@mui/material';
 import tokens from './data';
 import { useState } from 'react';
+import CustomSelect from '../_components/CustomSelect/CustomSelect';
+import { getNetwork } from '@wagmi/core';
+import oneInchSwap from '../_scripts/swap';
+
+const CustomTextField = ({ onChange }: { onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void }) => {
+  return (
+    <TextField
+      inputMode="decimal"
+      placeholder="0.0"
+      type="number"
+      onChange={onChange}
+      sx={{
+        width: '400px',
+        '& .MuiOutlinedInput-root': { borderRadius: '8px' },
+        '& .MuiOutlinedInput-input': { textAlign: 'right', padding: '10px 6px' },
+      }}
+    />
+  );
+};
 
 const Swap = () => {
   const [transferFrom, setTransferFrom] = useState('BTC');
+
   const onChangeTransferFrom = (e: SelectChangeEvent) => {
     setTransferFrom(e.target.value);
   };
@@ -17,16 +37,29 @@ const Swap = () => {
       <PageTitle title={'Swap'} />
       <Card sx={{ maxWidth: 600 }}>
         <CardContent>
-          <Box display={'flex'} justifyContent={'space-between'} alignItems="center">
-            <Select value={transferFrom} onChange={onChangeTransferFrom}>
-              {tokens.map((token) => (
-                <MenuItem key={token.symbol} value={token.symbol}>
-                  {token.name}
-                </MenuItem>
-              ))}
-            </Select>
-            <TextField />
-          </Box>
+          <Stack spacing={2}>
+            <Box display={'flex'} justifyContent={'space-between'} alignItems="center">
+              <CustomSelect value={transferFrom} onChange={onChangeTransferFrom}>
+                {tokens.map((token) => (
+                  <MenuItem key={token.symbol} value={token.symbol}>
+                    {token.name}
+                  </MenuItem>
+                ))}
+              </CustomSelect>
+              <CustomTextField />
+            </Box>
+            <Divider />
+            <Box display={'flex'} justifyContent={'space-between'} alignItems="center">
+              <CustomSelect value={transferFrom} onChange={onChangeTransferFrom}>
+                {tokens.map((token) => (
+                  <MenuItem key={token.symbol} value={token.symbol}>
+                    {token.name}
+                  </MenuItem>
+                ))}
+              </CustomSelect>
+              <CustomTextField />
+            </Box>
+          </Stack>
         </CardContent>
       </Card>
     </Box>
